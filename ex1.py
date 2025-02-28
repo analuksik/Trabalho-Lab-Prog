2a14a4d023fc0dd6234a4930b60db3d29e5c7168
Ana Clara Luksik <167776937+analuksik@users.noreply.github.com>
2025-02-28 07:41:46 -0300

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,10 @@
+import math
+
+r = float(input("Digite o valor do raio (r): "))
+if r < 1:
+  print(f"Valor inválido! O raio deve ser maior que zero.")
+else:
+  area = 4 * math.pi * (r**2)
+  print(f"O valor da área da esfera de raio {r} é de: {area:.2f}")
+  volume = 4/3 * math.pi * (r**3)
+  print(f"O valor do volume da esfera de raio {r} é de: {volume:.2f}")
