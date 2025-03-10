7f8f8d8d03c0e44313b1837f4028933cf95a462d
Ana Clara Luksik <167776937+analuksik@users.noreply.github.com>
2025-03-10 14:51:17 -0300

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,12 +1,12 @@
 try:
   h = float(input("Digite a sua altura: "))
-  s = str(input("Digite seu sexo (h) ou (m): "))
-  if s != 'h' and s != 'm':
-    print(f"Dados de entrada inválidos!")
-  elif s == 'h':
+  s = str(input("Digite seu sexo masculino (h) ou feminino (m): "))
+  if s == 'h':
     pIdeal = (72.7 * h) - 58
-  else:
+  elif s == 'm':
     pIdeal = (62.1 * h) - 44.7
-  print(f"O seu peso ideal é de: {pIdeal:.2f}")
+    print(f"O seu peso ideal é de: {pIdeal:.2f}")
+  else:
+    print("ERRO: DADOS DE ENTRADA")
 except:
   print("ERRO: ERRO DE EXCEÇÃO")
