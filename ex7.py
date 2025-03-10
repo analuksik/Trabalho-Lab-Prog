b50e83f41eccf9ecd6539505236376043d5c31c5
Ana Clara Luksik <167776937+analuksik@users.noreply.github.com>
2025-03-10 16:17:17 -0300

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -1,7 +1,10 @@
-preco = float(input("Digite o preço da mercadoria: "))
-reaj = str(input("Você deseja o valor com acréscimo ou com desconto? "))
-if reaj == 'acrescimo':
-  precoFinal = preco * 1.03
-elif reaj == 'desconto':
-  precoFinal = preco * 0.97
-print(f"O valor da mercadoria é de: R${precoFinal:.2f} ")
+try:
+  preco = float(input("Digite o preço da mercadoria: "))
+  reaj = str(input("Você deseja o valor com acréscimo ou com desconto? "))
+  if reaj == 'acrescimo':
+    precoFinal = preco * 1.03
+  elif reaj == 'desconto':
+    precoFinal = preco * 0.97
+  print(f"O valor da mercadoria é de: R${precoFinal:.2f} ")
+except:
+  print("ERRO: ERRO DE EXCEÇÃO!")
